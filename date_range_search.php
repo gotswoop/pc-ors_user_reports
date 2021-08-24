2430f551f048499bc38ca123ea161edfceade6cb
Swoop <swaroop.samek@gmail.com>
2021-08-23 22:20:57 -0700

    First commit of code from UTHealth

---
+++ date_range_search.php
@@ -0,0 +1,171 @@
+<?php
+// Call the REDCap Connect file in the main "redcap" directory.
+require_once '../redcap_connect.php';
+
+$users = REDCap::getUsers();
+REDCap::allowUsers ( $users );
+
+if (isset($_POST['startDate'])) {
+
+    $startdate = $_POST['startDate'];
+    $endate = $_POST['endDate'];
+
+	// SQL Query using Paramaterized inputs for date range to avoid SQL injection.
+    $query = "SELECT distinct a.record, b.value as FirstName, c.value as LastName, a.value as DateOfService
+FROM redcap.redcap_data a
+left join redcap.redcap_data b on a.project_id = b.project_id and a.record = b.record and b.field_name = 'patient_first'
+left join redcap.redcap_data c on a.project_id = c.project_id and a.record = c.record and c.field_name = 'patient_last'
+where a.field_name in ('billing_date', 'billing_date2', 'billing_date3', 'billing_date4', 'billing_date5', 'billing_date6')
+and a.project_id = 7217
+and cast(a.value as date) >= ? and cast(a.value as date) <= ?";
+
+    $table = "<thead>
+    <tr>
+    <th>  Record ID </th>
+    <th>  First Name </th>
+    <th>  Last Name </th>
+    <th>  Date of Service</th>
+    </tr></thead><tbody>";
+
+    $stmt = mysqli_prepare($conn, "$query");
+    mysqli_stmt_bind_param($stmt, "ss", $startdate, $endate);
+    $stmt->execute();
+    $stmt->bind_result($record_id, $first_name, $last_name, $dateofservice);
+    while($stmt->fetch()){
+        $table .= "<tr><td><a href='https://redcap.med.usc.edu/redcap_v11.2.2/DataEntry/record_home.php?pid=7217&arm=1&id=" . $record_id . "'> "  . $record_id . "</a></td><td>" . $first_name . "</td><td>" . $last_name . "</td><td>" . $dateofservice. "</td>";
+    }
+
+    $table .= "</tbody>";
+
+    echo $table;
+    exit;
+}
+
+?>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <script type='text/javascript' src='https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js'></script>
+
+    <link rel='stylesheet' type='text/css' href='https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css'>
+
+    <div class="page-header">
+
+    </div>
+
+    <div class="container">
+
+        <div id="wait" style="display:none;width:150px;height:150px;border:1px solid black;position:absolute;top:25%;left:35%;padding:2px; background: white"><img src='loading.gif' width="150" height="150" /><br><text style='text-align: center'>Loading..</text></div>
+
+        <form>
+                <label for="daterange" class="col-sm-2 col-form-label" style="text-align: right; font-size: large">Date Range:</label>
+                <div class="col-md-4">
+                    <div id="daterange" class="form-control" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc;">
+                        <i class="fa fa-calendar"></i>&nbsp;
+                        <span></span> <i class="fa fa-caret-down"></i>
+                    </div>
+                </div>
+        </form>
+        <br/>
+        <br/>
+    </div>
+
+    <div class="container">
+        <br/>
+        <br/>
+        <table id="table" class="table">
+            <thead>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <script type="text/javascript">
+
+            $(document).ajaxStart(function () {
+                $("#wait").css("display", "block");
+            });
+            $(document).ajaxComplete(function () {
+                $("#wait").css("display", "none");
+            });
+
+        function populateTable(table, datatable){
+
+            $('#table').DataTable().destroy();
+
+            var tbody = document.getElementById('table');
+
+            console.log(table);
+
+            tbody.innerHTML = table;
+            
+            $('#table').DataTable().draw();
+
+        }
+
+            var start = moment().subtract(29, 'days');
+            var end = moment();
+
+            function cb(start, end) {
+                $('#daterange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+            }
+
+            $('#daterange').daterangepicker({
+                startDate: start,
+                endDate: end,
+                ranges: {
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Calendar Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Calendar Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                }
+            }, cb);
+
+            cb(start, end);
+
+            var datatable = $('#table').DataTable();
+
+            $.ajax({
+                type: "POST",
+                dataType: "text",
+                data: {
+                    startDate: $('#daterange').data('daterangepicker').startDate.format('YYYY-MM-DD'),
+                    endDate: $('#daterange').data('daterangepicker').endDate.format('YYYY-MM-DD')
+                },
+                success: function(data) {
+                    populateTable(data, datatable);
+                }
+                });
+
+        $('#daterange').on('apply.daterangepicker', function(ev, picker) {
+            $.ajax({
+                type: "POST",
+                dataType: "text",
+                data: {
+                    startDate: $('#daterange').data('daterangepicker').startDate.format('YYYY-MM-DD'),
+                    endDate: $('#daterange').data('daterangepicker').endDate.format('YYYY-MM-DD')
+                },
+                success: function(data) {
+                    populateTable(data, datatable);
+                }
+            });
+        });
+
+    </script>
---
+++ orslist.php
@@ -0,0 +1,48 @@
+<?php
+// Call the REDCap Connect file in the main "redcap" directory. 
+
+require_once '../redcap_connect.php';
+
+REDCap::allowProjects(7217);
+
+// PID is hardcoded to avoid SQL injection - No variables are passed to SQL query; TODO: change to parameterized sql query with dyanamic pid.
+$query = "
+select record, max(patient_last), max(patient_first), max(room), max(surgeons), 'ORS'
+from (select record, 
+case WHEN redcap_data.field_name = 'patient_last' then value end as patient_last, 
+case WHEN redcap_data.field_name = 'patient_first' then value end as patient_first,
+case WHEN redcap_data.field_name = 'room' then value end as room,
+case WHEN redcap_data.field_name = 'surgeons' then SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(redcap_metadata.element_enum, CONCAT(' ', value, ','), -1), '\\\\n', 1), '1,', -1) end as surgeons,
+case WHEN redcap_data.field_name = 'service_list' then value end as service_list,
+redcap_metadata.element_enum
+from redcap_data
+left join redcap_metadata on redcap_data.project_id = '7217' and redcap_data.field_name = redcap_metadata.field_name 
+where redcap_data.project_id = '7217' 
+and redcap_data.field_name in ('surgeons', 'service_list', 'room', 'registry_id', 'patient_last', 'patient_first')
+and record in (select record from redcap_data where field_name = 'service_list' and value = 1)
+and record in (select record from redcap_data where field_name = 'patient_status' and value = 0)
+) a
+group by record
+order by max(patient_last)";
+
+$result = mysqli_query($conn, $query);
+
+echo "<table style='border-collapse: collapse; border: 1px solid black;'><tr>
+<th style=\"border: 1px solid black\">Record_ID</th>
+<th style=\"border: 1px solid black\">Patient Last</th>
+<th style=\"border: 1px solid black\">Patient First</th>
+<th style=\"border: 1px solid black\">Room</th>
+<th style=\"border: 1px solid black\">Surgeon</th>
+<th style=\"border: 1px solid black\">Service_List</th>
+</tr>";
+while ($row = mysqli_fetch_assoc($result)) {
+    echo '<tr>';
+    foreach ($row as $field) {
+        echo '<td style="border: 1px solid black">' . htmlspecialchars($field) . '</td>';
+    }
+    echo '</tr>';
+}
+echo '</table>';
+
+
+// OPTIONAL: Display the project footer
